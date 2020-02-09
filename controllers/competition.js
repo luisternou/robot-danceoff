77bc5fab729972c6d7a429785ac384961126347e
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-02-09 22:01:03 +0200

    added random avatars on homepage

--- controllers/competition.js
+++ controllers/competition.js
@@ -4,14 +4,59 @@
 
 module.exports = {
   getTeams: async (req, res) => {
-    try {
-      query = req.query
-      res.render('competition', { indexCSS: true, query})
-    } catch (err) {
-      return console.log(err)
+
+    let robotIds = []
+
+    for (let index = 0; index < 10; index++) {
+      let currentNumber = Math.ceil(Math.random() * 40)
+      if (robotIds.includes(currentNumber)) {
+        currentNumber = Math.ceil(Math.random() * 40)
+      }
+      robotIds[index] = currentNumber
+    
+      
     }
+    
+    function getRobot(id) {
+  return new Promise((resolve, reject) => {
+    https.get('https://challenge.parkside-interactive.com/api/robots/' + id, response => {
+      let data = "";
+      response.on('data', chunk => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        let robot = JSON.parse(data);
+        resolve(robot);
+      })
+
+    }).end();
+  });
+
+
+}
+
+    
+   
+      query = req.query
+
+      let teamOne = []
+
+      teamTwo = []
+      res.render('competition', { indexCSS: true, query, teamOne, teamTwo})
+   
   }
 
 }
 
   
+
+
+  // for (let index = 0; index < 5; index++) {
+  //   teamOne[index] = await getRobot(robotIds[index])
+    
+  // }
+
+  //   for (let index = 5; index < 9; index++) {
+  //   teamTwo[index] = await getRobot(robotIds[index])
+    
+  // }
--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,48 @@
 
-// Include models
 
-
+ const https = require('https');
+let avatarOne = "";
 module.exports = {
   getHome: async (req, res) => {
-    try {
-      res.render('index', { indexCSS: true})
-    } catch (err) {
-      return console.log(err)
-    }
+   
+
+let firstRandomId = Math.ceil(Math.random() * 40)
+let secondRandomId = Math.ceil(Math.random() * 40)
+
+if (secondRandomId === firstRandomId){
+	  secondRandomId = Math.ceil(Math.random() * 40)
+	}
+
+
+function getRobotAvatar(randomId) {
+  return new Promise((resolve, reject) => {
+    https.get('https://challenge.parkside-interactive.com/api/robots/' + randomId, response => {
+      let data = "";
+      response.on('data', chunk => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        let avatarOne = JSON.parse(data).avatar;
+        resolve(avatarOne);
+      })
+
+    }).end();
+  });
+
+
+}
+
+(async() => {
+  let avatarOne = await getRobotAvatar(firstRandomId);
+  let avatarTwo = await getRobotAvatar(secondRandomId);
+
+  res.render('index', {
+    indexCSS: true,
+    avatarOne,
+    avatarTwo
+  });
+})();
+
   }
 
 }
