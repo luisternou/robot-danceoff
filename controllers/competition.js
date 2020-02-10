c697125db18302e6d905784f43e593200b2b8336
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-02-10 06:27:20 +0200

    removed out of order robots

--- controllers/competition.js
+++ controllers/competition.js
@@ -1,25 +1,36 @@
 
 // Include models
-
-
+ 
+const https = require('https');
 module.exports = {
   getTeams: async (req, res) => {
+console.log(req.query);
 
+let query = req.query
+
+ 
+ 
     let robotIds = []
 
-    for (let index = 0; index < 10; index++) {
+    function getRobotIds(outoforder) {
+      let robotIds = []
+      for (let index = 0; index < 10; index++) {
       let currentNumber = Math.ceil(Math.random() * 40)
-      if (robotIds.includes(currentNumber)) {
+      if (robotIds.includes(currentNumber) | outoforder.includes(currentNumber) ) {
         currentNumber = Math.ceil(Math.random() * 40)
       }
       robotIds[index] = currentNumber
     
       
+      }
+      return robotIds
     }
+
+   
     
-    function getRobot(id) {
+    function getRobots() {
   return new Promise((resolve, reject) => {
-    https.get('https://challenge.parkside-interactive.com/api/robots/' + id, response => {
+    https.get('https://challenge.parkside-interactive.com/api/robots/' , response => {
       let data = "";
       response.on('data', chunk => {
         data += chunk;
@@ -35,14 +46,58 @@ module.exports = {
 
 }
 
-    
+
+
    
-      query = req.query
+ 
+ 
 
-      let teamOne = []
+    
+(async() => {
 
-      teamTwo = []
-      res.render('competition', { indexCSS: true, query, teamOne, teamTwo})
+
+
+let robots = await getRobots();
+
+let brokenRobots = [];
+
+for (let robot = 0; robot < robots.length; robot++) {
+  if (robots[robot].outOfOrder === true) {
+    brokenRobots.push(robots[robot].id)
+  }
+  
+}
+
+console.log(brokenRobots)
+
+let robotIds = getRobotIds(brokenRobots)
+
+let teamOne = []
+for (let index = 0; index < 5; index++) {
+  teamOne[index] = robots[robotIds[index]]
+  
+}
+
+
+
+let teamTwo = [];
+for (let index = 5; index < 10; index++) {
+   teamTwo[index] = robots[robotIds[index]];
+  
+}
+
+
+  
+  
+  res.render('competition', {
+    indexCSS: true,
+    teamOne,
+    query,
+    teamTwo
+    
+    
+  });
+})();
    
   }
 
@@ -50,13 +105,3 @@ module.exports = {
 
   
 
-
-  // for (let index = 0; index < 5; index++) {
-  //   teamOne[index] = await getRobot(robotIds[index])
-    
-  // }
-
-  //   for (let index = 5; index < 9; index++) {
-  //   teamTwo[index] = await getRobot(robotIds[index])
-    
-  // }
--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,14 +1,38 @@
 
 // Include models
 
-
+const https = require('https');
 module.exports = {
   getLeaderboard: async (req, res) => {
-    try {
-      res.render('leaderboard', { indexCSS: true})
-    } catch (err) {
-      return console.log(err)
-    }
+    
+      function getLeaderboardData() {
+  return new Promise((resolve, reject) => {
+    https.get('https://challenge.parkside-interactive.com/api/danceoffs/populated/', response => {
+      let data = "";
+      response.on('data', chunk => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        let leaderboard = JSON.parse(data);
+        resolve(leaderboard);
+      })
+
+    }).end();
+  });
+
+
+}
+
+(async() => {
+  let leaderboardData = await getLeaderboardData();
+  
+
+  res.render('leaderboard', {
+    indexCSS: true,
+    leaderboardData
+  });
+})();
+    
   }
 
 }
